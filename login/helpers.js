89f818788b15f5f786e91763e08b6a8be8c6850f
Zachary Bialik <zachbialik1207@gmail.com>
2021-03-05 16:24:35 -0500

    updated

--- login/helpers.js
+++ login/helpers.js
@@ -1,31 +1,10 @@
 
 var methods = {
     http_request: function(full_url, http_method='get', payload={} ) {
-
-      // TRYING AXIOS
+      
         const axios = require('axios')
 
         return axios.post(full_url, payload).then(response => response.data)
-        
-        // let res = null;
-
-        // if (http_method == 'get') {
-        //   res = await axios.get(full_url);
-        // } else if (http_method == 'post') {
-        //   res = await axios.post(full_url, payload);
-        // } else if (http_method == 'put') {
-        //   res = await axios.put(full_url, payload);
-        // } else if (http_method == 'delete') {
-        //   res = await axios.delete(full_url, payload);
-        // }
-
-
-        // try {
-        //   const {data:response} = await axios.post(full_url, payload) //use data destructuring to get data from the promise object
-        //   return response
-        // } catch (error) {
-        //   console.log(error);
-        // }
 
     }
 };
