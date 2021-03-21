a9e03f9b7d43220a2448722093a5b9b136fd5df8
Zachary Bialik <zachbialik1207@gmail.com>
2021-03-21 09:26:17 -0400

    added CORS headers

--- ui/src/app/game/game-backend.service.ts
+++ ui/src/app/game/game-backend.service.ts
@@ -1,5 +1,13 @@
-import { HttpClient } from '@angular/common/http';
-// import {Http, Headers, BaseRequestOptions, } from '@angular/http';
+import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 
+    'Access-Control-Allow-Origin':'*',
+    'Authorization':'authkey',
+    'userid':'1'
+  })
+};
+
 import { Injectable } from '@angular/core';
 import { timer } from 'rxjs';
 import { BehaviorSubject, Observable, Subject, throwError } from 'rxjs';
@@ -14,15 +22,16 @@ export class GameBackendService {
   gameUrl: string;
   gameData$: Observable<Game>;
   refreshInterval = 5000; // every 5 seconds
+  backend_url = "http://localhost:8080/games";
+  // backend_url = "__SERVER_IP__";
 
   constructor(private http: HttpClient) {
-    const server_ip = "__SERVER_IP__";
 
-    this.gameUrl = `http://${server_ip}:8080/games/1/`; // TODO include game identifier
+    this.gameUrl = `${this.backend_url}/1/`; // TODO include game identifier
 
     this.gameData$ = timer(1, this.refreshInterval).pipe( // will constanctly check the backend for updates to game data
-      switchMap(() => http.get<Game>(this.gameUrl))
+      switchMap(() => http.get<Game>(this.gameUrl, httpOptions))
     );
-
+    
   }
 }
