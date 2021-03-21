ff63bfcabc42e08c73cf398a04e4440d47f51f6f
Zachary Bialik <zachbialik1207@gmail.com>
2021-03-21 15:52:31 -0400

    fixing

--- ui/start_angular_server.sh
+++ ui/start_angular_server.sh
@@ -2,4 +2,4 @@
 
 EC2_PUBLIC_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 
-sed -i "s/__BACKEND_IP__/${EC2_PUBLIC_IP}/g" ./environments/environment.ts
+sed -i "s/__BACKEND_IP__/${EC2_PUBLIC_IP}/g" ./src/environments/environment.ts
