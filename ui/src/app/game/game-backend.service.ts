12cbde6f397498adfb266ed39bd52aeac534b4b5
Zachary Bialik <zachbialik1207@gmail.com>
2021-03-21 10:16:23 -0400

    try again

--- ui/src/app/game/game-backend.service.ts
+++ ui/src/app/game/game-backend.service.ts
@@ -1,11 +1,9 @@
 import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
 
-const backend_url = `http://localhost:8080/games`;
-
 const httpOptions = {
   headers: new HttpHeaders({ 
     'Access-Control-Allow-Origin':'*',
-    'Origin': `http://localhost:4200`
+    'Origin': "http://localhost:4200"
   })
 };
 
@@ -23,10 +21,10 @@ export class GameBackendService {
   gameUrl: string;
   gameData$: Observable<Game>;
   refreshInterval = 5000; // every 5 seconds
+  backend_url = `http://localhost:8080/games`;
 
   constructor(private http: HttpClient) {
-
-    this.gameUrl = `${backend_url}/1/`; // TODO include game identifier
+    this.gameUrl = `${this.backend_url}/1/`; // TODO include game identifier
 
     this.gameData$ = timer(1, this.refreshInterval).pipe( // will constanctly check the backend for updates to game data
       switchMap(() => http.get<Game>(this.gameUrl, httpOptions))
