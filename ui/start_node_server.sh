1991cc8ea7869a624ef6dcfd7db27753a6b56e20
Zachary Bialik <zachbialik1207@gmail.com>
2021-03-21 10:13:49 -0400

    remove unneeded script

--- ui/start_node_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-EC2_PUBLIC_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
-sed -i "s/__SERVER_IP__/${EC2_PUBLIC_IP}/g" ./src/app/game/game-backend.service.ts
-
-ng serve --host 0.0.0.0 --port 4200
