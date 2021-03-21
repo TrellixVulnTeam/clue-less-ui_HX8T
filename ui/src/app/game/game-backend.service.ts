c267f5596655e22d0760315aeb3dc4b995c3106f
Zachary Bialik <zachbialik1207@gmail.com>
2021-03-21 11:47:44 -0400

    test

--- ui/src/app/game/game-backend.service.ts
+++ ui/src/app/game/game-backend.service.ts
@@ -21,9 +21,13 @@ export class GameBackendService {
   gameData$: Observable<Game>;
   refreshInterval = 5000; // every 5 seconds
   backend_url = `http://localhost:8080/games`;
+  backend_ip: Observable<Object>;
 
   constructor(private http: HttpClient) {
-    this.gameUrl = `${this.backend_url}/1/`; // TODO include game identifier
+
+    this.backend_ip = http.get(`http://169.254.169.254/latest/meta-data/public-ipv4`);
+
+    this.gameUrl = `${this.backend_ip}/1/`; // TODO include game identifier
 
     this.gameData$ = timer(1, this.refreshInterval).pipe( // will constanctly check the backend for updates to game data
       switchMap(() => http.get<Game>(this.gameUrl, httpOptions))
