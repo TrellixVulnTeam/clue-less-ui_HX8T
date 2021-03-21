bcfe8abc6674a0c06cfc9f3d668a57ad10e130c9
Zachary Bialik <zachbialik1207@gmail.com>
2021-03-21 15:53:42 -0400

    agin

--- ui/start_angular_server.sh
+++ ui/start_angular_server.sh
@@ -3,3 +3,5 @@
 EC2_PUBLIC_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 
 sed -i "s/__BACKEND_IP__/${EC2_PUBLIC_IP}/g" ./src/environments/environment.ts
+
+ng serve --host 0.0.0.0 --port 4200
