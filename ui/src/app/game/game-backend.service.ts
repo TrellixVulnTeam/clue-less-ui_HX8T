c37c065fddfe04404b1136f888f0f3bbde71cc83
Zachary Bialik <zachbialik1207@gmail.com>
2021-03-20 20:31:16 -0400

    trying

--- ui/src/app/game/game-backend.service.ts
+++ ui/src/app/game/game-backend.service.ts
@@ -16,10 +16,21 @@ export class GameBackendService {
   refreshInterval = 5000; // every 5 seconds
 
   constructor(private http: HttpClient) {
-    this.gameUrl = "http://localhost:8080/games/1/"; // TODO include game identifier
+    const publicIp = require('public-ip');
+
+    (async () => {
+      console.log(await publicIp.v4());
+      //=> '46.5.21.123'
+  
+      console.log(await publicIp.v6());
+      //=> 'fe80::200:f8ff:fe21:67cf'
+    })();
+
+    this.gameUrl = `http://${publicIp.v4()}:8080/games/1/`; // TODO include game identifier
 
     this.gameData$ = timer(1, this.refreshInterval).pipe( // will constanctly check the backend for updates to game data
       switchMap(() => http.get<Game>(this.gameUrl))
     );
+
   }
 }
