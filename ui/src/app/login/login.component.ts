665dbc5c0e28a158c291182888dabff25b3a763e
Zachary Bialik <zachbialik1207@gmail.com>
2021-03-24 17:20:57 -0400

    added function to redirect to /game

--- ui/src/app/login/login.component.ts
+++ ui/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+  }
+
+  // action on join game
+  createGameButtonClick=() => {
+    this.router.navigateByUrl('/game');
+  };
+
+  // action on join game
+  joinGameButtonClick=() => {
+    this.router.navigateByUrl('/game');
+  };
 
   ngOnInit(): void {
   }
-
+  
 }
