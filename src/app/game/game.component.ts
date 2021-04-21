d94e4220255606b6dc90a6979576d46c4fa30be1
Zachary Bialik <zachbialik1207@gmail.com>
2021-04-21 17:01:39 -0400

    setup data template

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -3,33 +3,49 @@ import { Component, OnInit } from '@angular/core';
 import { PlayerService } from '../player-service/player.service';
 import { ActivatedRoute } from '@angular/router';
 
+
 @Component({
   selector: 'game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
-  
+
+  // game data
   gameId: any | undefined;
+  eventMessage: string | undefined
+  suggestion: any | undefined;
+  characters: any | undefined;
+  active: boolean | undefined;
+
+  // player data
+  player: any | undefined;
+  playerName: string | undefined;
   charName: any | undefined;
-  playerName: any | undefined;
-  playerData: any | undefined;
 
-  game: any | undefined;
-
-  constructor(private route: ActivatedRoute, private playerService: PlayerService) {} 
+  constructor(private route: ActivatedRoute, private playerService: PlayerService) {
+    this.gameId = route.snapshot.paramMap.get('gameId');
+    this.charName = route.snapshot.paramMap.get('charName');
+  }
 
   ngOnInit() {
-
-    this.gameId = this.route.snapshot.paramMap.get('gameId');
-    this.charName = this.route.snapshot.paramMap.get('charName');
-
+    
     this.playerService.setPlayerService(this.gameId, this.playerName);
 
     this.playerService.gameData$
-    .subscribe(data => { // sets up the subscription for game data (this is refreshed every 5 seconds in game-backend.service)
-        this.game = data;
-        this.playerData = data.characterMap.this.charName
-    });
+      .subscribe(data => { // sets up the subscription for game data (this is refreshed every 5 seconds in game-backend.service)
+        this.eventMessage = data.eventMessage;
+        this.active = data.active;
+        this.suggestion = data.suggestionCards;
+        this.characters = data.characters;
+
+        data.characters.forEach((character: any) => {
+          if (character.characterName == this.charName) {
+            this.player = character;
+            this.charName = this.player.characterName;
+          }
+        })
+
+      });
   }
 }
