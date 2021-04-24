f62555019a5156fd0d49c2373e8629c84e7a0687
Zachary Bialik <zachbialik1207@gmail.com>
2021-04-24 16:50:41 -0400

    pushing Game UI layout

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -3,6 +3,7 @@ import { PlayerService } from '../player-service/player.service';
 import { ActivatedRoute } from '@angular/router';
 import { Clue } from '../clue';
 import { LocationButton } from '../location-button';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'game',
@@ -13,7 +14,7 @@ export class GameComponent extends Clue implements OnInit {
 
   // game data
   gameId: any | undefined;
-  eventMessage: string | undefined
+  eventMessage: string | undefined;
   suggestion: any | undefined;
   characters: any | undefined;
   active: boolean | undefined;
@@ -23,51 +24,103 @@ export class GameComponent extends Clue implements OnInit {
   playerName: string | undefined;
   charName: any | undefined;
 
-  constructor(private route: ActivatedRoute, private playerService: PlayerService) {
+  constructor(private route: ActivatedRoute, private playerService: PlayerService, private formBuilder: FormBuilder) {
     super();
     this.gameId = route.snapshot.paramMap.get('gameId');
     this.charName = route.snapshot.paramMap.get('charName');
   }
 
-  ngOnInit() {
+  // refresh frontend data 
+  refreshData(data: any) {
+    this.eventMessage = data.eventMessage;
+    this.active = data.active;
+    this.suggestion = data.suggestionCards;
+    this.characters = data.characters;
 
-    // set re-used variables
-    var isPossibleMove:boolean;
+    data.characters.forEach((character: any) => {
+      var isPossibleMove: boolean;
+      
+      if (character.characterName == this.charName) {
+        this.player = character;
+        this.charName = this.player.characterName;
+
+        // check to see if each location is in user's possibleMoves and set to enabled if so
+        for (let locButton of this.locationButtons) {
+          isPossibleMove = false;
+          character.possibleMoves.forEach((location: any) => {
+            if (locButton.label == location.name) {
+              isPossibleMove = true;
+            }
+          });
+
+          if (isPossibleMove) {
+            // console.log(`enabling button: ${locButton.label}`)
+            locButton.enable()
+          } else {
+            // console.log(`disabling button: ${locButton.label}`)
+            locButton.disable()
+          }
+        }
+      }
+    })
+  }
+
+  // show game message
+  showGameMessage() {
+    // Get the snackbar DIV
+    var x:any = document.getElementById("gameMessage");
+  
+    // Add the "show" class to DIV
+    x.className = "show";
+  
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  }
+
+  // move  location
+  moveLocation(locName: string) {
+
+    console.log(`submitting post request to move to location: ${locName}`)
+
+    // update plater position
+    this.playerService.httpPostToBackend(`/${this.gameId}/location?playerName=${this.player.playerName}&charName=${this.charName}&locName=${locName}`).subscribe(
+      data => {
+        this.refreshData(data);
+      },
+      error => {
+        console.log("ERROR:", error);
+      },
+      () => {
+        console.log("POST for moving location is completed");
+      })
+  };
+
+  // complete turn
+  completeTurn() {
+
+    console.log(`submitting post request to move to complete-turn`)
+
+    // update plater position
+    this.playerService.httpPostToBackend(`/${this.gameId}/complete-turn?playerName=${this.player.playerName}&charName=${this.charName}`).subscribe(
+      data => {
+        this.refreshData(data);
+      },
+      error => {
+        console.log("ERROR:", error);
+      },
+      () => {
+        console.log("POST for completing turn is completed");
+      })
+  };
+
+  // start page
+  ngOnInit() {
 
     this.playerService.setPlayerService(this.gameId, this.playerName);
 
     this.playerService.gameData$
       .subscribe(data => { // sets up the subscription for game data (this is refreshed every 5 seconds in game-backend.service)
-        this.eventMessage = data.eventMessage;
-        this.active = data.active;
-        this.suggestion = data.suggestionCards;
-        this.characters = data.characters;
-
-        data.characters.forEach((character: any) => {
-          if (character.characterName == this.charName) {
-            this.player = character;
-            this.charName = this.player.characterName;
-
-            // check to see if each location is in user's possibleMoves and set to enabled if so
-            for (let locButton of this.locationButtons) {
-              isPossibleMove = false;
-              character.possibleMoves.forEach((location: any) => {
-                if (locButton.label == location.name) {
-                  isPossibleMove = true;
-                }
-              });
-
-              if (isPossibleMove) {
-                console.log(`${locButton.label} button is attempting to get enabled`)
-                locButton.enable()
-              } else {
-                console.log(`${locButton.label} button is being disabled`)
-                locButton.disable()
-              }
-            }
-          }
-        })
-
+        this.refreshData(data);
       });
   }
 }
